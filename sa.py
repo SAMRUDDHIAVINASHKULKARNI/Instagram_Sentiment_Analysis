7f7ea4c185b44e71ee6dbc1534d3229fca4bc45b
SAMRUDDHI KULKARNI <66955538+SAMRUDDHIAVINASHKULKARNI@users.noreply.github.com>
2022-02-26 22:01:28 +0530

    Update sa.py

--- sa.py
+++ sa.py
@@ -1,8 +1,14 @@
+#pandas library provides an easy to use data structures for data analysis
 import pandas as pd
+
+#nltk is a library which is used to process human language
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-nltk.downloader.download('vader_lexicon')
+
+#Vader Lexicon takes into account the intensity of the sentiment
+nltk.downloader.download('vader_lexicon') 
 file='/Insta_Data.xlsx'
+#Convert the excel sheet to dataframe
 xl = pd.ExcelFile(file)   #read Excel File
 dfs= xl.parse(xl.sheet_names[0])    #Parsing the excel sheet to dataframe
 dfs = list(dfs['Posts']) #Removes the blank rows from the dataframe
